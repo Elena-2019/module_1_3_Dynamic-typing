215d407fd96fdd5cd2808090e08abed6495a0c8e
Elena-2019 <loy-iver@inbox.ru>
2024-08-11 02:14:35 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,9 @@
 name='Elena'
 age=37
+new_age=age+1
 is_student=True
 
 print('Name: '+name)
 print('Age: '+str(age))
-print(str('New Age: ')+str(int((age)+1)))
+print('New Age: '+str(int(new_age)))
 print('Is Student: '+str(is_student))
